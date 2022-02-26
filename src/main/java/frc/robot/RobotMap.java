0b6366582d567d283e92512423f3dce52c2df557
LukeS26 <LukeS062022@gmail.com>
2022-02-26 14:52:49 -0500

    changed robotmap can ids

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -17,15 +17,15 @@ public class RobotMap {
   public static final int XBOX_PORT = 0;
   public static final int SWITCHBOX_PORT = 1;
 
-  public static final int FRONT_LEFT_PIVOT = 6;
-  public static final int FRONT_RIGHT_PIVOT = 5;
-  public static final int REAR_LEFT_PIVOT = 7;
-  public static final int REAR_RIGHT_PIVOT = 4;
+  public static final int FRONT_LEFT_PIVOT = 11;
+  public static final int FRONT_RIGHT_PIVOT = 12;
+  public static final int REAR_LEFT_PIVOT = 10; // NOT SET, was 7
+  public static final int REAR_RIGHT_PIVOT = 13;
 
-  public static final int FRONT_LEFT_DRIVE = 2;
-  public static final int FRONT_RIGHT_DRIVE = 1;
-  public static final int REAR_LEFT_DRIVE = 3;
-  public static final int REAR_RIGHT_DRIVE = 20;
+  public static final int FRONT_LEFT_DRIVE = 1;
+  public static final int FRONT_RIGHT_DRIVE = 20;
+  public static final int REAR_LEFT_DRIVE = 18;
+  public static final int REAR_RIGHT_DRIVE = 19;
 
   public static final int FRONT_LEFT_DIGITAL_INPUT = 2;
   public static final int FRONT_RIGHT_DIGITAL_INPUT = 1;
@@ -35,18 +35,18 @@ public class RobotMap {
   public static final int INTAKE_DRIVE = 9;
   public static final int INTAKE_POSITION = 8;
 
-  public static final int INDEXER_FLOOR = 10;
+  public static final int INDEXER_FLOOR = 4;
   public static final int INDEXER_SIDE = 18;
-  public static final int INDEXER_FEEDER = 11;
+  public static final int INDEXER_FEEDER = 5;
 
-  public static final int SHOOTER_LEADER = 13;
-  public static final int SHOOTER_FOLLOWER = 12;
+  public static final int SHOOTER_LEADER = 7;
+  public static final int SHOOTER_FOLLOWER = 6;
 
   public static final int HOOD_SERVO_1 = 0;
   public static final int HOOD_SERVO_2 = 1;
 
-  public static final int ELEVATOR_LEADER = 14;
-  public static final int ELEVATOR_FOLLOWER1 = 15;
-  public static final int ELEVATOR_FOLLOWER2 = 16;
-  public static final int PIVOT_MOTOR = 17;
+  public static final int ELEVATOR_LEADER = 15;
+  public static final int ELEVATOR_FOLLOWER1 = 16;
+  public static final int ELEVATOR_FOLLOWER2 = 17;
+  public static final int PIVOT_MOTOR = 14;
 }
