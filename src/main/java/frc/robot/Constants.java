ab56059965b363810fd5efe9ff88118bfb6fc6b5
LukeS26 <LukeS062022@gmail.com>
2022-03-24 07:45:20 -0400

    fixed constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,12 +70,12 @@ public class Constants {
   public static final double SHOOTER_FF = 1.0 / 4400.0;
 
   public static final double INTAKE_P = 0.03;
-  public static final double INTAKE_I = 0.0;
-  public static final double INTAKE_D = 0.0;
+  public static final double INTAKE_I = 0.001;
+  public static final double INTAKE_D = 0.03;
 
   public static final double PIVOT_P = 0.075;
-  public static final double PIVOT_I = 0.00001;
-  public static final double PIVOT_D = 0.05;
+  public static final double PIVOT_I = 0;
+  public static final double PIVOT_D = 0;
 
   public static final double ELEVATOR_P = 0.1;
   public static final double ELEVATOR_I = 0.0001;
